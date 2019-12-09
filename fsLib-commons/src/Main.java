b1af99ebce22f6c2f92832db59ab06d2a4e428b9
汐榊少爷 <xmh.yxw@gmail.com>
2019-12-09 16:32:18 +0800

    更新

--- fsLib-commons/src/Main.java
+++ fsLib-commons/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //方法 一
-      System.out.println(System.currentTimeMillis());
+     // System.out.println(System.currentTimeMillis());
 
 //方法 三
-        System.out.println(new Date().getTime());
+        //System.out.println(new Date().getTime());
 
 //        String html = HttpUtil.sendGet("http://www.neeu.com/news/fashion/","gbk");
 //        Document doc = Jsoup.parse(html);
@@ -25,6 +25,19 @@ public class Main {
 //            Elements elements = e.select("h3").select("a[href]");
 //            String se = elements.attr("href");
 //            int i = 0;
+        System.out.println("args = [" + args + "]");
+        jsonParam.put("pay_ver","100");
+        jsonParam.put("pay_type","020");
+        jsonParam.put("service_id","012");
+        jsonParam.put("merchant_no",merchant_no);
+        jsonParam.put("terminal_id",terminal_id);
+        jsonParam.put("terminal_time",terminal_time);
+        jsonParam.put("total_fee",total_fee);
+        jsonParam.put("order_body","");
+        jsonParam.put("open_id",open_id);
+        jsonParam.put("terminal_trace",times);
+        jsonParam.put("sub_appid","");
+
         }
     }
 
